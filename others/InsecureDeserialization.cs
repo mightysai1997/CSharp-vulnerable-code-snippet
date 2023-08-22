83f6393330df9105de999695b6f3f666bd45e9af
qafdevsec <111730727+qafdevsec@users.noreply.github.com>
2023-08-22 23:11:08 +0530

    Update InsecureDeserialization.cs

--- others/InsecureDeserialization.cs
+++ others/InsecureDeserialization.cs
@@ -1,57 +1,12 @@
-using System;
-using System.IO;
-using System.Runtime.Serialization.Json;
-using System.Text;
+using System.Web.UI.WebControls;
+using System.Web.Script.Serialization;
 
-public class DeserializationHelper
+class Bad
 {
-    public static void SecureDataContractJsonDeserialization(string type, string json)
+    public static object Deserialize(TextBox textBox)
     {
-        // Validate the type parameter to ensure it's coming from a trusted source
-        // and corresponds to a safe type for deserialization
-        if (!IsValidType(type))
-        {
-            Console.WriteLine("Invalid type parameter");
-            return;
-        }
-
-        // Define a list of known types for deserialization
-        Type[] knownTypes = GetKnownTypes();
-
-        // Configure DataContractJsonSerializerSettings with known types
-        DataContractJsonSerializerSettings dataContractJsonSerializerSettings = new DataContractJsonSerializerSettings
-        {
-            KnownTypes = knownTypes
-        };
-
-        // Use a secure JSON deserializer library like System.Text.Json
-        try
-        {
-            MemoryStream memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(json));
-            DataContractJsonSerializer dataContractJsonSerializer = new DataContractJsonSerializer(Type.GetType(type), dataContractJsonSerializerSettings);
-            var deserializedObject = dataContractJsonSerializer.ReadObject(memoryStream);
-            memoryStream.Close();
-
-            // Process the deserialized object as needed
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e);
-        }
-    }
-
-    // Example method to validate the type parameter
-    private static bool IsValidType(string type)
-    {
-        // Implement your validation logic here
-        // Return true if the type is valid, false otherwise
-        return true;
-    }
-
-    // Example method to provide known types for deserialization
-    private static Type[] GetKnownTypes()
-    {
-        // Return an array of known safe types for deserialization
-        return new Type[] { typeof(KnownType1), typeof(KnownType2) };
+        JavaScriptSerializer serializer = new JavaScriptSerializer(new SimpleTypeResolver());
+        // BAD
+        return serializer.DeserializeObject(textBox.Text);
     }
 }
