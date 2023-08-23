363f6aa8094a44f7ac046ccb0c9a3fdf6dd379db
qafdevsec <111730727+qafdevsec@users.noreply.github.com>
2023-08-23 23:29:10 +0530

    Update InsecureDataTransmission.cs

--- others/InsecureDataTransmission.cs
+++ others/InsecureDataTransmission.cs
@@ -1,27 +1,15 @@
-using System.Text;
-using System.Web;
-using System.Web.Security;
-
-public class CleartextStorageHandler : IHttpHandler
+public class FooBarController : Controller
 {
-
-    public void ProcessRequest(HttpContext ctx)
+    [HttpPost] // Noncompliant
+    [ValidateInput(false)]
+    public ActionResult Purchase(string input)
     {
-        string accountName = ctx.Request.QueryString["AccountName"];
-        // BAD: Setting a cookie value with cleartext sensitive data.
-        ctx.Response.Cookies["AccountName"].Value = accountName;
-        // GOOD: Encoding the value before setting it.
-        ctx.Response.Cookies["AccountName"].Value = Protect(accountName, "Account name");
+        return Foo(input);
     }
 
-    /// <summary>
-    /// Protect the cleartext value, using the given type.
-    /// </summary>
-    /// <value>
-    /// The protected value, which is no longer cleartext.
-    /// </value>
-    public string Protect(string value, string type)
+    [HttpPost] // Noncompliant
+    public ActionResult PurchaseSomethingElse(string input)
     {
-        return Encoding.UTF8.GetString(MachineKey.Protect(Encoding.UTF8.GetBytes(value), type));
+        return Foo(input);
     }
 }
