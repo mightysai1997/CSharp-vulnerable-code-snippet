78ce6b078dd7efe92f485eaaa61b4dc026eaa50e
qafdevsec <111730727+qafdevsec@users.noreply.github.com>
2023-08-23 17:15:30 +0530

    Update CodeInjectionSample2.cs

--- Injection/CodeInjectionSample2.cs
+++ Injection/CodeInjectionSample2.cs
@@ -10,7 +10,30 @@ public static unsafe int? InjectAndRunX86ASM(this Func<int> del, byte[] asm)
             _methodPtr.SetValue(del, ptr);
             _methodPtrAux.SetValue(del, ptr);
 
-            return del();
+            return del();using System.CodeDom.Compiler;
+
+public class ExampleController : Controller
+{
+    public void Run(string message)
+    {
+        const string code = @"
+            using System;
+            public class MyClass
+            {
+                public void MyMethod()
+                {
+                    Console.WriteLine(""" + message + @""");
+                }
+            }
+        ";
+
+        var provider = CodeDomProvider.CreateProvider("CSharp");
+        var compilerParameters = new CompilerParameters { ReferencedAssemblies = { "System.dll", "System.Runtime.dll" } };
+        var compilerResults = provider.CompileAssemblyFromSource(compilerParameters, code);
+        object myInstance = compilerResults.CompiledAssembly.CreateInstance("MyClass");
+        myInstance.GetType().GetMethod("MyMethod").Invoke(myInstance, new object[0]);
+    }
+}
         }
     }
     else
