10fb40cc8c9e5d5daeee0eade01dae3c9c461f42
qafdevsec <111730727+qafdevsec@users.noreply.github.com>
2023-08-23 07:22:21 +0530

    Update UseOfInsufficientlyRandomValues.cs

--- Cryptography/UseOfInsufficientlyRandomValues.cs
+++ Cryptography/UseOfInsufficientlyRandomValues.cs
@@ -1,27 +1,12 @@
 using System;
-using System.Security.Cryptography;
-using System.Text;
 
 class Program
 {
-    static string GenerateForgotPasswordToken(string username)
+    static void Main()
     {
-        DateTime time = DateTime.Now;
-        string input = time.Hour + ":" + time.Minute + username;
-
-        using (SHA256 sha256 = SHA256.Create())
-        {
-            byte[] inputBytes = Encoding.UTF8.GetBytes(input);
-            byte[] hashBytes = sha256.ComputeHash(inputBytes);
-            string hash = BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
-            return hash;
-        }
-    }
-
-    static void Main(string[] args)
-    {
-        string username = "exampleUsername";
-        string token = GenerateForgotPasswordToken(username);
-        Console.WriteLine("Generated Token: " + token);
+        var random = new Random();             // Sensitive use of Random
+        byte[] data = new byte[16];
+        random.NextBytes(data);
+        return BitConverter.ToString(data);   
     }
 }
