d386635a36dfb84ec7080a895825332f5d00e789
qafdevsec <111730727+qafdevsec@users.noreply.github.com>
2023-08-23 17:28:51 +0530

    Update CodeInjectionSample.cs

--- Injection/CodeInjectionSample.cs
+++ Injection/CodeInjectionSample.cs
@@ -10,7 +10,54 @@ public class CodeInjectionHandler : IHttpHandler
     {
         // Code for calculating tax is provided as unvalidated user input
         string taxFormula = ctx.Request.QueryString["tax_formula"];
-        // Used to create C#
+        // Used to create C#using Microsoft.CSharp;
+using System;
+using System.CodeDom.Compiler;
+using System.Reflection;
+using System.Text;
+using System.Web;
+
+public class CodeInjectionHandler : IHttpHandler
+{
+    public void ProcessRequest(HttpContext ctx)
+    {
+        // Code for calculating tax is provided as unvalidated user input
+        string taxFormula = ctx.Request.QueryString["tax_formula"];
+        
+        // Used to create C# source code
+        StringBuilder sourceCode = new StringBuilder();
+        sourceCode.Append("public class TaxCalc {\n");
+        sourceCode.Append("\tpublic int CalculateTax(int value) {\n");
+        sourceCode.Append("\t\treturn " + taxFormula + ";\n");
+        sourceCode.Append("\t}\n");
+        sourceCode.Append("}\n");
+
+        // BAD: This compiles the sourceCode, containing unvalidated user input
+        CSharpCodeProvider c = new CSharpCodeProvider();
+        ICodeCompiler icc = c.CreateCompiler();
+        CompilerParameters cp = new CompilerParameters();
+        CompilerResults cr = icc.CompileAssemblyFromSource(cp, sourceCode.ToString());
+
+        // Compiled input is loaded, and an instance of the class is constructed
+        Assembly a = cr.CompiledAssembly;
+        object taxCalc = a.CreateInstance("TaxCalc");
+
+        // Unsafe code is executed
+        Type taxCalcType = taxCalc.GetType();
+        MethodInfo mi = taxCalcType.GetMethod("CalculateTax");
+        int value = int.Parse(ctx.Request.QueryString["value"]);
+        int taxValue = (int)mi.Invoke(taxCalc, new object[] { value });
+
+        // Result is returned to the user
+        ctx.Response.Write("Tax value is: " + taxValue);
+    }
+
+    public bool IsReusable
+    {
+        get { return false; }
+    }
+}
+
         StringBuilder sourceCode = new StringBuilder("");
         sourceCode.Append("public class TaxCalc {\n");
         sourceCode.Append("\tpublic int CalculateTax(int value){\n");
