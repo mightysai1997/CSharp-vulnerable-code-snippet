a926b4bfe255a3e77f7c0171b04f0b4daf72f884
qafdevsec <111730727+qafdevsec@users.noreply.github.com>
2023-08-23 22:56:20 +0530

    Update InsecureDataTransmission.cs

--- others/InsecureDataTransmission.cs
+++ others/InsecureDataTransmission.cs
@@ -1,13 +1,27 @@
-public class UserController
+using System.Text;
+using System.Web;
+using System.Web.Security;
+
+public class CleartextStorageHandler : IHttpHandler
 {
-    public void Login(string username, string password)
+
+    public void ProcessRequest(HttpContext ctx)
     {
-        // This code sends the password over HTTPS, which is a secure protocol.
-        // An attacker cannot intercept the password.
-        var client = new HttpsClient();
-        var request = new HttpRequest("https://example.com/api/login");
-        request.SetParameter("username", username);
-        request.SetParameter("password", password);
-        client.SendAsync(request).Wait();
+        string accountName = ctx.Request.QueryString["AccountName"];
+        // BAD: Setting a cookie value with cleartext sensitive data.
+        ctx.Response.Cookies["AccountName"].Value = accountName;
+        // GOOD: Encoding the value before setting it.
+        ctx.Response.Cookies["AccountName"].Value = Protect(accountName, "Account name");
+    }
+
+    /// <summary>
+    /// Protect the cleartext value, using the given type.
+    /// </summary>
+    /// <value>
+    /// The protected value, which is no longer cleartext.
+    /// </value>
+    public string Protect(string value, string type)
+    {
+        return Encoding.UTF8.GetString(MachineKey.Protect(Encoding.UTF8.GetBytes(value), type));
     }
 }
