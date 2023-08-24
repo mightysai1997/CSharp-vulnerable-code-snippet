8a1a18e22238ad3376a6f1914d3430800189b2d5
qafdevsec <111730727+qafdevsec@users.noreply.github.com>
2023-08-24 23:06:50 +0530

    Update SqlInjectionSample1.cs

--- Injection/SqlInjectionSample1.cs
+++ Injection/SqlInjectionSample1.cs
@@ -1,22 +1,34 @@
-public void Foo(DbContext context, string query, string param)
-{
-    string sensitiveQuery = string.Concat(query, param);
-    context.Database.ExecuteSqlCommand(sensitiveQuery); // Sensitive
-    context.Query<User>().FromSql(sensitiveQuery); // Sensitive
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 
-    context.Database.ExecuteSqlCommand($"SELECT * FROM mytable WHERE mycol={value}", param); // Sensitive, the FormattableString is evaluated and converted to RawSqlString
-    string query = $"SELECT * FROM mytable WHERE mycol={param}";
-    context.Database.ExecuteSqlCommand(query); // Sensitive, the FormattableString has already been evaluated, it won't be converted to a parametrized query.
+public class ExampleClass
+{
+    public void Foo(DbContext context, string query, string param)
+    {
+        string sensitiveQuery = string.Concat(query, param);
+        context.Database.ExecuteSqlCommand(sensitiveQuery); // Sensitive
+        context.Query<User>().FromSql(sensitiveQuery); // Sensitive
+
+        context.Database.ExecuteSqlCommand($"SELECT * FROM mytable WHERE mycol={value}", param); // Sensitive, the FormattableString is evaluated and converted to RawSqlString
+        string formattedQuery = $"SELECT * FROM mytable WHERE mycol={param}";
+        context.Database.ExecuteSqlCommand(formattedQuery); // Sensitive, the FormattableString has already been evaluated, it won't be converted to a parametrized query.
+    }
+
+    public void Bar(SqlConnection connection, string param)
+    {
+        SqlCommand command;
+        string sensitiveQuery = string.Format("INSERT INTO Users (name) VALUES (\"{0}\")", param);
+        command = new SqlCommand(sensitiveQuery); // Sensitive
+
+        command.CommandText = sensitiveQuery; // Sensitive
+
+        SqlDataAdapter adapter;
+        adapter = new SqlDataAdapter(sensitiveQuery, connection); // Sensitive
+    }
 }
 
-public void Bar(SqlConnection connection, string param)
+public class User
 {
-    SqlCommand command;
-    string sensitiveQuery = string.Format("INSERT INTO Users (name) VALUES (\"{0}\")", param);
-    command = new SqlCommand(sensitiveQuery); // Sensitive
-
-    command.CommandText = sensitiveQuery; // Sensitive
-
-    SqlDataAdapter adapter;
-    adapter = new SqlDataAdapter(sensitiveQuery, connection); // Sensitive
+    // Define User class properties here
 }
