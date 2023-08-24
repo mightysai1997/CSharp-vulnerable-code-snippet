507f4dbf49258bc16f91283ffc78c9a32565d961
qafdevsec <111730727+qafdevsec@users.noreply.github.com>
2023-08-24 23:29:39 +0530

    Update UseOfInsufficientlyRandomValues.cs

--- Cryptography/UseOfInsufficientlyRandomValues.cs
+++ Cryptography/UseOfInsufficientlyRandomValues.cs
@@ -1,55 +1,9 @@
 using System;
 
-namespace InsufficientRandomnessExample
+class ExampleClass
 {
-    public class InsecureRandom
+    public void ExampleMethod(Random random)
     {
-        private int seed;
-
-        public InsecureRandom(int seed)
-        {
-            this.seed = seed;
-        }
-
-        public int Next()
-        {
-            // Insecure random number generation using a linear congruential generator
-            seed = (seed * 1664525 + 1013904223) & 0x7FFFFFFF;
-            return seed;
-        }
-    }
-
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            int userChoice;
-            Console.WriteLine("Select an option:\n1. Roll Dice\n2. Generate OTP");
-            if (int.TryParse(Console.ReadLine(), out userChoice))
-            {
-                if (userChoice == 1)
-                {
-                    // Simulating a dice roll
-                    InsecureRandom random = new InsecureRandom(42);
-                    int diceRoll = random.Next() % 6 + 1;
-                    Console.WriteLine($"You rolled a {diceRoll}");
-                }
-                else if (userChoice == 2)
-                {
-                    // Simulating generation of a one-time password (OTP)
-                    InsecureRandom random = new InsecureRandom(1337);
-                    int otp = random.Next() % 10000;
-                    Console.WriteLine($"Your OTP is: {otp:D4}");
-                }
-                else
-                {
-                    Console.WriteLine("Invalid choice.");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Invalid input.");
-            }
-        }
+        var sensitiveVariable = random.Next();
     }
 }
