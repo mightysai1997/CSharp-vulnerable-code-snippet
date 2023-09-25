d0f5b8ea337f6a2d5490504f061978086d8e2c41
qafdevsec <111730727+qafdevsec@users.noreply.github.com>
2023-09-25 14:52:51 +0530

    Update Use-of-Hardcoded-Credentials.cs

--- PasswordManagement/Use-of-Hardcoded-Credentials.cs
+++ PasswordManagement/Use-of-Hardcoded-Credentials.cs
@@ -1,19 +1,41 @@
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 
-public class MyUserManager
+public class User
 {
-    private UserManager<ApplicationUser> userManager;
+    public string UserName { get; set; }
+    public int UserId { get; set; }
 
-    public MyUserManager()
+    public User(string userName, int userId)
     {
-        var userStore = new UserStore<ApplicationUser>(new ApplicationDbContext());
-        userManager = new UserManager<ApplicationUser>(userStore);
+        UserName = userName;
+        UserId = userId;
     }
+}
 
-    public IdentityResult CreateUser(string username, string password)
+public class UserManager
+{
+    public User CreateUser(string userName, int userId)
     {
-        var user = new ApplicationUser { UserName = username };
-        return userManager.Create(user, password);
+        // You can add your logic here to create a user with the provided parameters
+        User newUser = new User(userName, userId);
+
+        // Example: You can save the user to a database or perform other operations
+
+        return newUser;
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        UserManager userManager = new UserManager();
+        
+        string userName = "JohnDoe";
+        int userId = 12345;
+        
+        User newUser = userManager.CreateUser(userName, userId);
+        
+        Console.WriteLine($"User Created: UserName - {newUser.UserName}, UserID - {newUser.UserId}");
     }
 }
