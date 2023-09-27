bf0f3ba95f3f9c19eb707cac32b346ecb77f0e68
qafdevsec <111730727+qafdevsec@users.noreply.github.com>
2023-09-27 15:12:31 +0530

    Update UseOfHardcodedCredentials.cs

--- PasswordManagement/UseOfHardcodedCredentials.cs
+++ PasswordManagement/UseOfHardcodedCredentials.cs
@@ -1,72 +1,25 @@
 using System;
-using System.Collections.Generic;
-
-public class User
-{
-    public string Username { get; set; }
-    public string Password { get; set; }
-    // Add other user properties as needed
-}
-
-public class UserManager
-{
-    private List<User> users = new List<User>();
-
-    public User CreateUser(string username)
-    {
-        // Check if the username already exists
-        if (UserExists(username))
-        {
-            throw new InvalidOperationException("Username already exists.");
-        }
-
-        // Hardcoded password for the user
-        string hardcodedPassword = "password123";
-
-        // Create a new user and add it to the list
-        var newUser = new User
-        {
-            Username = username,
-            Password = hardcodedPassword,
-            // Initialize other user properties here
-        };
-
-        users.Add(newUser);
-
-        return newUser;
-    }
-
-    public bool UserExists(string username)
-    {
-        // Check if a user with the given username already exists
-        return users.Any(u => u.Username == username);
-    }
-
-    // Add other methods for managing users, such as GetUserById, UpdateUser, DeleteUser, etc.
-}
 
 class Program
 {
     static void Main()
     {
-        var userManager = new UserManager();
+        // Sample input data
+        string username = "john_doe";
+        string email = "john.doe@example.com";
 
-        try
-        {
-            // Attempt to create a new user
-            var newUser = userManager.CreateUser("john_doe");
+        // Call the CreateUser function with two parameters
+        CreateUser(username, email);
 
-            // Check if the user was created successfully
-            if (newUser != null)
-            {
-                Console.WriteLine("User created successfully.");
-                Console.WriteLine($"Username: {newUser.Username}");
-                Console.WriteLine($"Password: {newUser.Password}");
-            }
-        }
-        catch (InvalidOperationException ex)
-        {
-            Console.WriteLine($"Error: {ex.Message}");
-        }
+        // Rest of your code...
+    }
+
+    // Define the CreateUser function with two parameters
+    static void CreateUser(string username, string email)
+    {
+        // Your logic for creating a user goes here
+        Console.WriteLine($"Creating user with username: {username} and email: {email}");
+        
+        // For example, you can make an API call or perform database operations here
     }
 }
