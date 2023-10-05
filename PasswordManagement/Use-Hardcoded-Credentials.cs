f5c24281522777bf6ae49f03deb3682019ea1299
qafdevsec <111730727+qafdevsec@users.noreply.github.com>
2023-10-05 11:58:16 +0530

    Update Use-Hardcoded-Credentials.cs

--- PasswordManagement/Use-Hardcoded-Credentials.cs
+++ PasswordManagement/Use-Hardcoded-Credentials.cs
@@ -1,49 +1,36 @@
 using System;
-using System.Configuration;
 using System.Web.Security;
 
 public class UserRegistration
 {
-    public bool RegisterUser()
+    public static void Main(string[] args)
     {
+        string username = "exampleUser"; // Replace with the desired username
+        string password = "examplePassword"; // Replace with the desired password
+
         try
         {
-            // Retrieve credentials from configuration file
-            string username = ConfigurationManager.AppSettings["MembershipUsername"];
-            string password = ConfigurationManager.AppSettings["MembershipPassword"];
-
-            // Validate input username and password (ensure they are not null or empty)
-            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
-            {
-                Console.WriteLine("Invalid username or password.");
-                return false;
-            }
-
             // Attempt to create the user using Membership.CreateUser method
             MembershipUser newUser = Membership.CreateUser(username, password);
 
             if (newUser != null)
             {
-                // User registration successful
-                return true;
+                Console.WriteLine("User registration successful!");
             }
             else
             {
-                // User registration failed
-                return false;
+                Console.WriteLine("User registration failed.");
             }
         }
         catch (MembershipCreateUserException ex)
         {
             // Handle specific exceptions if needed
             Console.WriteLine("User registration failed. Error: " + ex.Message);
-            return false;
         }
         catch (Exception ex)
         {
             // Handle other exceptions (e.g., database connection issues, etc.)
             Console.WriteLine("Error: " + ex.Message);
-            return false;
         }
     }
 }
