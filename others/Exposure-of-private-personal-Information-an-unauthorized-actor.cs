8f6f7690583b98fb4f40f4166b5a173c6fd29bc8
qafdevsec <111730727+qafdevsec@users.noreply.github.com>
2023-10-07 11:39:52 +0530

    Update Exposure-of-private-personal-Information-an-unauthorized-actor.cs

--- others/Exposure-of-private-personal-Information-an-unauthorized-actor.cs
+++ others/Exposure-of-private-personal-Information-an-unauthorized-actor.cs
@@ -1,15 +1,45 @@
 using System;
-using System.Net;
+using System.IO;
 
-public class DataTransmitter
+public class CredentialWriter
 {
     public static void Main(string[] args)
     {
-        string sensitiveData = "ThisIsSensitiveData123";
+        WriteCredentials();
+        Console.WriteLine("Credentials written to file.");
+    }
 
-        // Vulnerable: Sending sensitive data over HTTP (cleartext)
-        WebClient client = new WebClient();
-        string response = client.UploadString("http://example.com/submit", sensitiveData);
-        Console.WriteLine("Server Response: " + response);
+    public static void WriteCredentials()
+    {
+        string password = "cleartext password";
+        string filePath = "credentials.txt";
+
+        try
+        {
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                // BAD: Writing password to disk in cleartext
+                writer.WriteLine(password);
+
+                // Alternatively, you can hash the password before storing it
+                // string hashedPassword = HashFunction(password);
+                // writer.WriteLine(hashedPassword);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+    }
+
+    // HashFunction is a placeholder for a secure hashing algorithm
+    // For example, you can use SHA-256 hashing
+    public static string HashFunction(string password)
+    {
+        using (System.Security.Cryptography.SHA256 sha256 = System.Security.Cryptography.SHA256.Create())
+        {
+            byte[] hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+            return BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
+        }
     }
 }
