00060a0c14a4d0377b618a9a187b545b877976dd
qafdevsec <111730727+qafdevsec@users.noreply.github.com>
2023-10-07 10:50:16 +0530

    Update ExposureOfPrivatePersonalInformationToAnUnauthorizedActorSample2.cs

--- others/ExposureOfPrivatePersonalInformationToAnUnauthorizedActorSample2.cs
+++ others/ExposureOfPrivatePersonalInformationToAnUnauthorizedActorSample2.cs
@@ -1,14 +1,42 @@
-using System.Net.Sockets;
+using System;
 
-class TestSocket
+public class UserProfile
 {
-    public static void Run()
-    {
-        // Sensitive
-        Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+    private string username;
+    private string password;
 
-        // TcpClient and UdpClient simply abstract the details of creating a Socket
-        TcpClient client = new TcpClient("example.com", 80); // Sensitive
-        UdpClient listener = new UdpClient(80); // Sensitive
+    public UserProfile(string username, string password)
+    {
+        this.username = username;
+        this.password = password;
+    }
+
+    public void DisplayUserProfile()
+    {
+        Console.WriteLine("Username: " + username);
+        Console.WriteLine("Password: " + password);
+    }
+}
+
+public class UnauthorizedAccess
+{
+    public static void Main(string[] args)
+    {
+        // Simulating a scenario where a user's profile is accessed without proper authorization.
+        string unauthorizedUsername = "hacker";
+        string unauthorizedPassword = "password123";
+
+        // Assume userProfile is an instance of the UserProfile class, holding private user information.
+        UserProfile userProfile = new UserProfile("legitimateUser", "securePassword");
+
+        // Unauthorized actor accesses and displays the user's profile.
+        if (unauthorizedUsername.Equals(userProfile.Username, StringComparison.OrdinalIgnoreCase) && unauthorizedPassword.Equals(userProfile.Password))
+        {
+            userProfile.DisplayUserProfile(); // Vulnerability: Unauthorized access to sensitive information
+        }
+        else
+        {
+            Console.WriteLine("Unauthorized access: Invalid credentials.");
+        }
     }
 }
