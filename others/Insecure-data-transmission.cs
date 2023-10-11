d732bdaaabe52e57229aa9e105bd75a0e0da0e0f
qafdevsec <111730727+qafdevsec@users.noreply.github.com>
2023-10-11 21:24:55 +0530

    Update Insecure-data-transmission.cs

--- others/Insecure-data-transmission.cs
+++ others/Insecure-data-transmission.cs
@@ -1,13 +1,48 @@
-using System.Text;
-using System.Web;
-using System.Web.Security;
+using System;
+using System.Net;
+using System.IO;
 
-public class PrivateInformationHandler : IHttpHandler
+class Program
 {
-
-    public void ProcessRequest(HttpContext ctx)
+    static void Main()
     {
-        string address = ctx.Request.QueryString["Address1"];
-        logger.Info("User has address: " + address);
+        string username = "myUsername";
+        string password = "myPassword";
+
+        // Insecure URL (HTTP instead of HTTPS)
+        string url = "http://example.com/login";
+
+        // Create a request object to send the data
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+        
+        // Set the request method to POST
+        request.Method = "POST";
+
+        // Prepare the data to be sent
+        string postData = $"username={username}&password={password}";
+        byte[] byteArray = System.Text.Encoding.UTF8.GetBytes(postData);
+        
+        // Set the content type of the request
+        request.ContentType = "application/x-www-form-urlencoded";
+
+        // Get the request stream and write the data to it
+        using (Stream dataStream = request.GetRequestStream())
+        {
+            dataStream.Write(byteArray, 0, byteArray.Length);
+        }
+
+        // Get the response from the server
+        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+        
+        // Read the response data
+        using (Stream responseStream = response.GetResponseStream())
+        {
+            StreamReader reader = new StreamReader(responseStream);
+            string responseString = reader.ReadToEnd();
+            Console.WriteLine("Response: " + responseString);
+        }
+
+        // Close the response and response stream
+        response.Close();
     }
 }
