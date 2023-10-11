c5f572e641d89fce6782b21950926bd1d7cf88d6
qafdevsec <111730727+qafdevsec@users.noreply.github.com>
2023-10-11 21:37:11 +0530

    Update Insecure-data-transmission.cs

--- others/Insecure-data-transmission.cs
+++ others/Insecure-data-transmission.cs
@@ -1,48 +1,11 @@
-using System;
-using System.Net;
 using System.IO;
 
-class Program
+public void Example()
 {
-    static void Main()
+    var tempPath = Path.GetTempFileName();  // Noncompliant
+
+    using (var writer = new StreamWriter(tempPath))
     {
-        string username = "myUsername";
-        string password = "myPassword";
-
-        // Insecure URL (HTTP instead of HTTPS)
-        string url = "http://example.com/login";
-
-        // Create a request object to send the data
-        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-        
-        // Set the request method to POST
-        request.Method = "POST";
-
-        // Prepare the data to be sent
-        string postData = $"username={username}&password={password}";
-        byte[] byteArray = System.Text.Encoding.UTF8.GetBytes(postData);
-        
-        // Set the content type of the request
-        request.ContentType = "application/x-www-form-urlencoded";
-
-        // Get the request stream and write the data to it
-        using (Stream dataStream = request.GetRequestStream())
-        {
-            dataStream.Write(byteArray, 0, byteArray.Length);
-        }
-
-        // Get the response from the server
-        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-        
-        // Read the response data
-        using (Stream responseStream = response.GetResponseStream())
-        {
-            StreamReader reader = new StreamReader(responseStream);
-            string responseString = reader.ReadToEnd();
-            Console.WriteLine("Response: " + responseString);
-        }
-
-        // Close the response and response stream
-        response.Close();
+        writer.WriteLine("content");
     }
 }
