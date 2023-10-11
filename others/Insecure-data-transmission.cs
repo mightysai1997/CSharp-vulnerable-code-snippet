ab25b6b7aac6235480a6e6cad222b868c993987a
qafdevsec <111730727+qafdevsec@users.noreply.github.com>
2023-10-11 22:17:49 +0530

    Update Insecure-data-transmission.cs

--- others/Insecure-data-transmission.cs
+++ others/Insecure-data-transmission.cs
@@ -1,17 +1,13 @@
-using System;
 using System.Text;
 using System.Web;
+using System.Web.Security;
 
 public class PrivateInformationHandler : IHttpHandler
 {
+
     public void ProcessRequest(HttpContext ctx)
     {
         string address = ctx.Request.QueryString["Address1"];
         logger.Info("User has address: " + address);
-    }
-
-    public bool IsReusable
-    {
-        get { return false; }
     }
 }
