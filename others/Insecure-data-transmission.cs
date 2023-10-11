ccc70f7f867c0c18d46856def40ef1e7b6bdaaf5
qafdevsec <111730727+qafdevsec@users.noreply.github.com>
2023-10-11 21:53:17 +0530

    Update Insecure-data-transmission.cs

--- others/Insecure-data-transmission.cs
+++ others/Insecure-data-transmission.cs
@@ -1,11 +1,22 @@
-using System.IO;
+using System;
+using System.Net;
 
-public void Example()
+class Program
 {
-    var tempPath = Path.GetTempFileName();  // Noncompliant
-
-    using (var writer = new StreamWriter(tempPath))
+    static void Main()
     {
-        writer.WriteLine("content");
+        string sensitiveData = "This is my sensitive information.";
+
+        // Insecure transmission over HTTP (without encryption)
+        string insecureUrl = "http://example.com/api";
+
+        // Create a WebClient instance to send the data
+        WebClient client = new WebClient();
+
+        // Transmit sensitive data over an insecure connection (HTTP)
+        string response = client.UploadString(insecureUrl, sensitiveData);
+
+        // Display the response from the server
+        Console.WriteLine("Server Response: " + response);
     }
 }
