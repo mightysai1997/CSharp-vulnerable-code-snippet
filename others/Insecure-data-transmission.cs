13532a916faa707ba5d5714fea8764384f7115e9
qafdevsec <111730727+qafdevsec@users.noreply.github.com>
2023-10-11 21:20:00 +0530

    Update Insecure-data-transmission.cs

--- others/Insecure-data-transmission.cs
+++ others/Insecure-data-transmission.cs
@@ -1,37 +1,13 @@
-using System;
-using System.Net.Http;
 using System.Text;
+using System.Web;
+using System.Web.Security;
 
-class Program
+public class PrivateInformationHandler : IHttpHandler
 {
-    static void Main()
+
+    public void ProcessRequest(HttpContext ctx)
     {
-        string sensitiveData = "ThisIsSensitiveData123";
-
-        using (HttpClient client = new HttpClient())
-        {
-            // Insecure HTTP URL (http:// instead of https://)
-            string insecureApiUrl = "http://example.com/api/data";
-
-            // Convert sensitive data to JSON
-            string jsonData = $"{{ \"data\": \"{sensitiveData}\" }}";
-
-            // Create StringContent with JSON data
-            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
-
-            // Send data over an insecure connection (HTTP)
-            HttpResponseMessage response = client.PostAsync(insecureApiUrl, content).Result;
-
-            if (response.IsSuccessStatusCode)
-            {
-                Console.WriteLine("Data sent successfully over an insecure connection.");
-            }
-            else
-            {
-                Console.WriteLine("Failed to send data over an insecure connection.");
-            }
-        }
-
-        Console.ReadLine();
+        string address = ctx.Request.QueryString["Address1"];
+        logger.Info("User has address: " + address);
     }
 }
